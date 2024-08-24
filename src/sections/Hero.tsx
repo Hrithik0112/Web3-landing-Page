02a14cff9e08ae50732518bebec760ffda4a3646
Hrithik <slumdog4321@gmail.com>
2024-08-24 11:31:07 +0530

    Feat : heor animation added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,11 +1,13 @@
+"use client"
 import Button from '@/components/Button'
 import React from 'react'
+import {motion} from "framer-motion"
 
 const Hero = () => {
   return (
     <section className="w-full flex flex-col md:flex-row items-center space-x-8">
             <div className=" md:w-1/2 flex-col flex flex-wrap gap-8">
-                <h1 className="md:text-8xl text-5xl">
+                <h1 className="lg:text-8xl text-5xl">
                     Discover Collect Sell Digital NFT.
                 </h1>
                 <span className="text-gray-400 leading-8 text-xl font-nominee">
@@ -17,11 +19,21 @@ const Hero = () => {
                     <Button outline={true}>Join Discord</Button>
                 </div>
             </div>
-            <div className="relative md:w-1/2 flex flex-col flex-wrap gap-4 items-center justify-center">
-                <img
+            <div className="relative md:w-1/2 flex flex-col flex-wrap gap-4 items-center justify-center md:mt-0 mt-5">
+                <motion.img
                     src="/assets/etherum.png"
                     alt="etherum_logo"
                     className="w-[86%] h-[70%] object-contain"
+                    animate={{
+                        translateY : ["-60px", "30px"],
+                        rotateY : 30,
+                    }}
+                    transition={{
+                        duration : 2,
+                        repeat :Infinity,
+                        repeatType :"mirror",
+                        ease:"linear"
+                    }}
                 />
             </div>
         </section>
