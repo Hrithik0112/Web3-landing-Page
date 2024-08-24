74555a81a01f4aa466f153cb3984b1ee22bbefac
Hrithik <slumdog4321@gmail.com>
2024-08-24 15:37:21 +0530

    Feat :Added Promotion section animation

--- src/sections/Promotion.tsx
+++ src/sections/Promotion.tsx
@@ -1,13 +1,50 @@
+"use client"
 import Button from '@/components/Button'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {motion, useAnimation} from "framer-motion"
 
 const Promotion = () => {
+
+    const [isMdScreen, setIsMdScreen] = useState(false);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMdScreen(window.innerWidth >= 768); // Tailwind's 'md' breakpoint is 768px
+    };
+
+    // Initial check
+    checkScreenSize();
+
+    // Add event listener to handle screen resize
+    window.addEventListener('resize', checkScreenSize);
+
+    return () => {
+      window.removeEventListener('resize', checkScreenSize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isMdScreen) {
+      controls.start({ x: 100 });
+    } else {
+      controls.stop();
+    }
+  }, [isMdScreen, controls]);
+
   return (
     <section className="relative w-full min-h-[400px] flex flex-row flex-wrap items-end justify-around gap-4">
-            <img
-                className="object-scale-down max-w-[200px] sm:max-w-sm max-h-[inherit] overflow-hidden"
+            <motion.img
+                className="object-scale-down max-w-[200px] sm:max-w-sm max-h-[inherit] overflow-hidden "
                 src="/assets/vr-person.webp"
                 alt="vr_person"
+                animate={controls}
+                transition={{
+                    duration: 3,
+                    repeat: Infinity,
+                    repeatType: "mirror",
+                    ease: "easeInOut"
+                }}
             />
             <div className="bg-gradient-to-bl from-sky-700 via-purple-700 to-blue-800 rounded-3xl p-3 md:p-0 w-full sm:h-[320px] h-full absolute bottom-0 z-[-1] shadow-lg shadow-purple-600/70"></div>
             <article className="flex flex-col flex-wrap gap-6 items-start justify-center h-[inherit] md:max-w-[40%] m-2 lg:-translate-y-10">
